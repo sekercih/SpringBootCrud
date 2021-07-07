28085d8e5641812b838d5e3c149f225286041030
sekercih <hkkskrc@gmail.com>
2021-07-07 03:26:31 +0300

    spring boot proje

---
+++ src/main/java/repository/KisiRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.Kisi;
+
+@Repository
+public interface KisiRepository extends JpaRepository<Kisi, Integer> {
+	
+	
+
+}
