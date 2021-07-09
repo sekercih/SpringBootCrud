7db2f43482f177bac7978401d6d8a9c0ead1e3c4
sekercih <hkkskrc@gmail.com>
2021-07-10 00:07:20 +0300

    path edit

--- src/main/java/controller/KisiController.java
+++ src/main/java/controller/KisiController.java
@@ -33,7 +33,7 @@ public class KisiController {
 	public Kisi yeniKisiEkle(@RequestBody Kisi kisi) {
 		return kisiService.kisiEkle(kisi);
 	}
-	@GetMapping(path="/{id}")
+	@GetMapping(path="/ara/{id}")
 	public Optional<Kisi> idIleKisiListele(@PathVariable Integer id) {
 		return kisiService.idIleKisiGetir(id);
 	}
